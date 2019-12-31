9af21316d031f11da0d371fb9aacd640727879c0
Michał Filipiuk <mkfilipiuk@gmail.com>
2019-12-31 14:50:25 +0100

    Added pytorch dataset

---
+++ DPOD/datasets/kaggle_dataset.py
@@ -0,0 +1,32 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+from skimage import io
+import numpy as np
+import os
+
+class kaggle_image_mask_dataset(Dataset):
+    
+    def __init__(self, path, is_train=True):
+        self.images_dir = os.path.join(path, "train_images" if is_train else "test_images")
+        self.masks_dir = os.path.join(path, "train_masks" if is_train else "test_masks")
+        data_csv = pd.read_csv(os.path.join(path, "train.csv"))
+        self.images_ID = data_csv.ImageId
+        self.predition_strings = data_csv.PredictionString
+
+    def __len__(self):
+        return len(self.images_ID)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_name = os.path.join(self.images_dir, self.images_ID[idx]+".jpg")
+        image = io.imread(img_name)
+        
+        mask_name = os.path.join(self.masks_dir, self.images_ID[idx]+".npy")
+        masks = np.load(mask_name)
+        
+        predition_string = self.predition_strings[idx]
+
+        return image, masks, predition_string
