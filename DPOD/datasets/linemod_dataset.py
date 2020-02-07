089ca9adb555dd1dc62557f2746c3d742043f8fc
Michał Filipiuk <mkfilipiuk@gmail.com>
2020-02-07 17:22:28 +0000

    linemod_dataset

--- DPOD/datasets/linemod_dataset.py
+++ DPOD/datasets/linemod_dataset.py
@@ -47,17 +47,20 @@ class LinemodImageMaskDataset(Dataset):
 
     Therefore element of dataset looks as follows:
     (image[H, W], (classification[N+1, H, W], u_channel[num_of_colors, H, W], v_channel[num_of_colors, H, W]), prediction_string)
+    
+    (image[H, W], (classification[N+1, H, W], u_channel[num_of_colors, H, W], v_channel[num_of_colors, H, W]), prediction_string)
     """
 
-    def __init__(self, path, is_train=True, num_of_colors=256, num_of_models=79, image_size=(640, 480), setup=None):
+    def __init__(self, path, is_train=True, num_of_colors=256, num_of_models=7, image_size=(640, 480), setup=None):
         """
         :param setup    one of ["train", "test", "val", "all"]
         """
         self.is_train = is_train
         self.image_size = image_size
-        #self.frequency_path = os.path.join(path, "frequency.npy")
-        self.images_dir = os.path.join(path, "train_images" if is_train else "test_images")
-        self.masks_dir = os.path.join(path, "train_targets" if is_train else "test_targets")
+        self.images_dir = os.path.join(path, "RGB-D", "rgb_noseg")
+        self.masks_dir = os.path.join(path, "masks")
+        self.frequency_path = os.path.join(path, "frequency.npy")
+        
         # This ifs result from backward compatibility
         if setup is not None:
             if setup == "train":
@@ -71,37 +74,36 @@ class LinemodImageMaskDataset(Dataset):
             else:
                 raise AttributeError(f"No such setup: {setup}")
         setup = "all" if setup is None else setup
+        
+        if self.is_train and setup == "all":
+            # trening i walidacja złączone
+            train_data_csv = pd.read_csv(os.path.join(path, "train_data_images_split.csv"))
+            val_data_csv = pd.read_csv(os.path.join(path, "validation_data_images_split.csv"))
             
-        if self.is_train:
-            data_csv = pd.read_csv(os.path.join(path, "train.csv"))
-            self.images_ID = data_csv.ImageId
-            self.prediction_strings = data_csv.PredictionString
-            edge = int(len(self.images_ID) * self.VAL_SIZE)
-            if setup == "train":
-                self.images_ID = self.images_ID[edge:]
-                self.prediction_strings = self.prediction_strings[edge:]
-            elif setup == "val":
-                self.images_ID = self.images_ID[:edge]
-                self.prediction_strings = self.prediction_strings[:edge]
-            self.prediction_strings = list(self.prediction_strings)
-        else:
-            self.images_ID = [x[:-4] for x in os.listdir(os.path.join(path, "test_images"))]
-            self.prediction_strings = None
-        self.images_ID = list(self.images_ID)
+            self.images_filenames = pd.concat([train_data_csv, val_data_csv]).filenames
+        if self.is_train and setup == "train":
+            # sam trening
+            self.images_filenames = pd.read_csv(os.path.join(path, "train_data_images_split.csv")).filenames
+        if self.is_train and setup == "val":
+            # sama walidacja
+            self.images_filenames = pd.read_csv(os.path.join(path, "validation_data_images_split.csv")).filenames
+        if not self.is_train:
+            # sam test
+            self.images_filenames = pd.read_csv(os.path.join(path, "test_data_images_split.csv")).filenames
 
         self.num_of_colors = num_of_colors
         self.num_of_models = num_of_models
+        
         self.im_transform = transforms.Compose([
-            transforms.Resize(image_size, Image.NEAREST),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225]),
         ])
         self.image_transform = transforms.Compose([
             transforms.ToPILImage(),
-            transforms.Resize(image_size, Image.NEAREST),
             transforms.ToTensor(),
         ])
+        self.images_filenames = np.array(self.images_filenames)
 
     def __len__(self):
         return len(self.images_ID)
@@ -110,28 +112,26 @@ class LinemodImageMaskDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        img_name = os.path.join(self.images_dir, self.images_ID[idx]+".jpg")
+        img_name = os.path.join(self.images_dir, self.images_filenames[idx])
         image = Image.open(img_name)
 
         image = self.im_transform(image)
         
-        if not self.is_train:
-            return image
+#         if not self.is_train:
+#             return image
 
-        mask_name = os.path.join(self.masks_dir, self.images_ID[idx]+".npy")
+        mask_name = os.path.join(self.masks_dir, self.images_filenames[idx][:-4][6:]+"_masks.npy")
         masks = np.load(mask_name)
         unknown_model_mask = masks[..., 0] >= self.num_of_models
         masks[unknown_model_mask, 0] = self.num_of_models
-        masks = cv2.resize(masks, dsize=self.image_size[::-1], interpolation=cv2.INTER_NEAREST)
+        #masks = cv2.resize(masks, dsize=self.image_size[::-1], interpolation=cv2.INTER_NEAREST)
         masks = torch.tensor(masks, dtype=torch.uint8)
 
-        prediction_string = self.prediction_strings[idx]
-
-        classification_mask = masks[..., 0]
-        height_mask         = masks[..., 1]
-        angle_mask          = masks[..., 2]
+        height_mask         = masks[0]
+        angle_mask          = masks[1]
+        classification_mask = masks[2]
         
-        return image, (classification_mask, height_mask, angle_mask), prediction_string
+        return image, (classification_mask, height_mask, angle_mask)
 
     def get_class_weights(self, force=False):
         if os.path.exists(self.frequency_path) and not force:
@@ -139,35 +139,48 @@ class LinemodImageMaskDataset(Dataset):
             frequency = np.load(self.frequency_path)
         else:
             print("Calculating frequency")
-            paths = [os.path.join(self.masks_dir, self.images_ID[idx]+".npy") for idx in range(len(self.images_ID))]
-            frequency = np.zeros(self.num_of_models+1)
+            list_of_train_masks = np.array(pd.read_csv(os.path.join(path, "train_data_images_split.csv")).filenames)
+            paths = [os.path.join(self.masks_dir, filename[:-4][6:] + "_masks.npy") for filename in list_of_train_masks]
+            class_frequency = np.zeros(self.num_of_models+1)
+            height_frequency = np.zeros(self.num_of_colors)
+            angle_frequency = np.zeros(self.num_of_colors)
 
             t = tqdm(total=len(self.images_ID))     
             with ProcessPoolExecutor() as executor:
                 for freq in executor.map(get_class_frequencies, paths, [self.num_of_models]*len(self.images_ID)):
-                    frequency += freq
+                    class_frequency += freq[0]
+                    height_frequency += freq[1]
+                    angle_frequency += freq[2]
                     t.update()
 
             np.save(self.frequency_path, frequency)
 
-        zeros = frequency == 0
-        frequency[zeros] = 1
-        weights = 1 / frequency
-        weights[zeros] = 0
-        weights *= (self.num_of_models + 1) / weights.sum()
-        return torch.FloatTensor(weights)
-    
-    def get_IDs(self):
-        return self.images_ID
         
-def get_class_frequencies(path, num_of_models):
+        results = []
+        for frequency in [class_frequency, height_frequency, angle_frequency]:
+            zeros = frequency == 0
+            frequency[zeros] = 1
+            weights = 1 / frequency
+            weights[zeros] = 0
+            weights *= (self.num_of_models + 1) / weights.sum()
+            results.append(weights)
+            
+        results = [torch.FloatTensor(r) for r in results]    
+        return results
+
+        
+def get_class_frequencies(path, num_of_models, num_of_colors):
         masks = np.load(path)
-        unknown_model_mask = masks[..., 0] >= num_of_models
-        masks[unknown_model_mask, 0] = num_of_models
-        frequency = np.zeros(num_of_models +1)
-        for i in np.unique(masks[..., 0]):
-            frequency[i] += (masks[..., 0] == i).sum()
-        return frequency
+        class_frequency = np.zeros(num_of_models +1)
+        height_frequency = np.zeros(num_of_colors)
+        angle_frequency = np.zeros(num_of_colors)
+        for i in np.unique(masks[2]):
+            class_frequency[i] += (masks[2] == i).sum()
+            
+        for i in range(num_of_colors):
+            height_frequency[i] += (masks[0] == i).sum()
+            angle_frequency[i] += (masks[1] == i).sum()
+        return class_frequency, height_frequency, angle_frequency
         
 
 
