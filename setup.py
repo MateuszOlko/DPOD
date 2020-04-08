834b322314c0ebc211f19234aff00fefdf563498
Mateusz Olko <mateusz.olko@gmail.com>
2020-04-08 10:46:09 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DPOD",
+    version="0.1",
+    packages=find_packages(include=('DPOD')),
+    python_requires=">=3.7",
+    install_requires=[],
+)
