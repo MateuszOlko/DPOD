b3142e7a77e6db37ca22618147ebb2015a93593f
Mateusz Olko <mateusz.olko@gmail.com>
2020-06-19 17:26:28 +0200

    Fix evaluation procedure

--- linemod_evaluate.py
+++ linemod_evaluate.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 import cv2
 
 from DPOD.datasets.linemod.models_handler import project_points
+from DPOD.datasets import PATHS
+from DPOD.datasets.linemod.models_handler import transform_points
 
 def error_on_instance(
     model_vertices,
@@ -18,10 +20,11 @@ def error_on_instance(
     translation_vector_ground_truth):
 
     # pprint([translation_vector, translation_vector_ground_truth])
-    pprint([rotation_matrix, rotation_matrix_ground_truth])
+    # pprint([rotation_matrix, rotation_matrix_ground_truth])
     # print('\n\n\n\n')
 
     vertices_predicted = model_vertices@rotation_matrix + translation_vector
+    # vertices_predicted = transform_points(model_vertices, rotation_matrix, translation_vector)
     vertices_ground_truth = model_vertices@rotation_matrix_ground_truth + translation_vector_ground_truth
 
     size0 = vertices_predicted[:, 0].max() - vertices_predicted[:, 0].min()
@@ -38,6 +41,12 @@ def error_on_instance(
     size2 = vertices_ground_truth[:, 2].max() - vertices_ground_truth[:, 2].min()
 
     size = (size0**2 + size1**2 + size2**2) ** (1/2)
+
+    size0 = model_vertices[:, 0].max() - model_vertices[:, 0].min()
+    size1 = model_vertices[:, 1].max() - model_vertices[:, 1].min()
+    size2 = model_vertices[:, 2].max() - model_vertices[:, 2].min()
+
+    size = (size0**2 + size1**2 + size2**2) ** (1/2)
     #print(model_vertices.shape, vertices_ground_truth.shape, vertices_predicted.shape)
     #print(((vertices_predicted - vertices_ground_truth)**2).sum(axis=1).shape)
     # print(vertices_ground_truth.mean(axis=0), vertices_predicted.mean(axis=0))
@@ -52,7 +61,7 @@ def error_on_instance(
 
     err_sq = ((vertices_predicted - vertices_ground_truth)**2)
     err = (err_sq.sum(axis=1)**(1/2))
-    err = (err**(1/2)).mean()
+    err = err.mean()
 
     hit = err < (size * 0.1)
     print(err, size * 0.1, hit)
@@ -63,79 +72,71 @@ def evaluate(path_to_linemod_dir, path_to_instances_dir):
     models_handler = ModelsHandler()
     instances_paths = glob(f'{path_to_instances_dir}/*.pkl')
     acc_on_images = []
+    retrived = 0
+    relevant = 0
+    retrived_relevant = 0
+    errors = []
 
-
-    translations = []
-    translations_gt = []
-    rotations = []
-    rotations_gt = []
+    idx= 0
     for instances_path in instances_paths:
         print('evaluating', instances_path)
 
         with open(instances_path, 'rb') as file:
             instances = pickle.load(file)
 
+        instances_dict = {name: (vec, matrix) for name, vec, matrix in instances}
+
         image_id = os.path.split(instances_path)[1][:-len('_instances.pkl')]
 
         hits = []
 
-        image = np.zeros((480, 640))
-        image_gt = np.zeros((480, 640))
-
-        for model_name, translation_vector, rotation_matrix in instances:
+        for model_name in models_handler.model_names:
             position_file_path = f'{path_to_linemod_dir}/poses/{model_name}/info_{image_id}.txt'
-            # translation_vector = translation_vector * np.array([1, 1, -1])
-            # rotation_matrix = rotation_matrix @ np.array([[-1, 0, 0], [0, 1, 0], [0, 0, 1]])
-            # rotation_matrix = rotation_matrix * np.array([[1, 1, 1], [1, -1, -1], [-1, -1, -1]])
-            if not os.path.exists(position_file_path):
-                hits.append(0)
-                print('wrongly predicted', model_name, position_file_path)
-                continue
-                
-        
-            position_ground_truth = read_position_file(position_file_path)
-            if position_ground_truth is None:
-                hits.append(0)
-                print('wrongly predicted', model_name, position_file_path)
-                continue
+            if model_name in instances_dict.keys():
+                retrived += 1
+                translation_vector, rotation_matrix = instances_dict[model_name]
+                translation_vector = translation_vector * np.array([1, -1, -1])
+                rotation_matrix = np.diag([1, -1, -1])@rotation_matrix
+                if not os.path.exists(position_file_path):
+                    hits.append(0)
+                    print('wrongly predicted', model_name, position_file_path)
+                    continue
 
-            _, _, rotation_matrix_gt, translation_vector_gt, _ = position_ground_truth
-            err, hit = error_on_instance(
-                models_handler.get_vertices(model_name),
-                rotation_matrix,
-                translation_vector,
-                rotation_matrix_gt,
-                translation_vector_gt
-            )
-            hits.append(hit)
-            # translations.append(translation_vector)
-            # translations_gt.append(translation_vector_gt)
-            # for i in range(3):
-            #     rotations.append(rotation_matrix[i])
-            #     rotations_gt.append(rotation_matrix_gt[i])
+                position_ground_truth = read_position_file(position_file_path)
+                if position_ground_truth is None:
+                    hits.append(0)
+                    print('wrongly predicted', model_name, position_file_path)
+                    continue
+                _, _, rotation_matrix_gt, translation_vector_gt, _ = position_ground_truth
+                err, hit = error_on_instance(
+                    models_handler.get_vertices(model_name),
+                    rotation_matrix,
+                    translation_vector,
+                    rotation_matrix_gt,
+                    translation_vector_gt
+                )
+                errors.append(err)
+                retrived_relevant += hit
+                relevant += 1
+                hits.append(hit)
+                # models_handler.draw_class_mask(image, model_name, rotation_matrix, translation_vector)
+                # models_handler.draw_class_mask(image_gt, model_name, rotation_matrix_gt, translation_vector_gt)
+            elif os.path.exists(position_file_path):
+                position_ground_truth = read_position_file(position_file_path)
+                if position_ground_truth is not None:
+                    hits.append(0)
+                    relevant += 1
+                    # _, _, rotation_matrix_gt, translation_vector_gt, _ = position_ground_truth
+                    # models_handler.draw_class_mask(image_gt, model_name, rotation_matrix_gt, translation_vector_gt)
 
-            models_handler.draw_class_mask(image, model_name, rotation_matrix, translation_vector)
-            models_handler.draw_class_mask(image_gt, model_name, rotation_matrix_gt, translation_vector_gt)
 
+        # plt.imsave(f"render{idx}.png", image)
+        # plt.imsave(f"render_gt{idx}.png", image_gt)
+        # idx += 1
+        #
+        # masks = np.load(f"{PATHS['linemod']}/masks/{image_id}_masks.npy")
+        # plt.imsave(f"class{idx}.png", masks[2])
 
-        # p = project_points(np.array([[0,0,0], [0.1,0,0]]), models_handler.camera_matrix)
-        # p = [(int(e[0]), int(e[1])) for e in p]
-        # print(p)
-        # cv2.line(image, p[0], p[1], (1, 1, 1))
-        # p = project_points(np.array([[0,0,0], [0,0.1,0]]), models_handler.camera_matrix)
-        # p = [(int(e[0]), int(e[1])) for e in p]
-        # print(p)
-        # cv2.line(image, p[0], p[1], (1, 1, 1))
-        # p = project_points(np.array([[0,0,0], [0,0,0.1]]), models_handler.camera_matrix)
-        # p = [(int(e[0]), int(e[1])) for e in p]
-        # print(p)
-        # cv2.line(image, p[0], p[1], (1, 1, 1))
-        plt.imsave("render.png", image)
-        plt.imsave("render_gt.png", image_gt)
-
-        masks = np.load(f"../experiments/repro/Apr-15-16:06/infered_masks/{image_id}_masks.npy")
-        plt.imsave("class.png", masks[0])
-        break
 
     # print("Solution")
     # print(len(translations))
@@ -143,17 +144,17 @@ def evaluate(path_to_linemod_dir, path_to_instances_dir):
     # print("Rotations")
     # print(len(translations))
     # print(np.linalg.lstsq(np.array(rotations), np.array(rotations_gt))[0])
-    
-    acc_on_images.append(np.mean(hits))
+        m = np.mean(hits) if hits else 0
+        acc_on_images.append(m)
 
-    return np.mean(acc_on_images)
+    return np.mean(acc_on_images), np.mean(errors)
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument('path_to_instances_dir')
-    argparser.add_argument('--path_to_linemod_dir', default='/mnt/bigdisk/datasets/linemod')
+    argparser.add_argument('--path_to_linemod_dir', default=PATHS["linemod"])
     args = argparser.parse_args()
 
-    acc = evaluate(args.path_to_linemod_dir, args.path_to_instances_dir)
-    print(acc)
+    acc, mean_error = evaluate(args.path_to_linemod_dir, args.path_to_instances_dir)
+    print(f"Accuracy {acc}; Mean error {mean_error}")
 
